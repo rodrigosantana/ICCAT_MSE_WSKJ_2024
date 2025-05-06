58787133b388c4540ba7af97fe2af22edc84ed83
Adrian Hordyk <ar.hordyk@gmail.com>
2025-05-06 14:31:24 -0300

    Create 000_load_openMSE_packages.R

---
+++ 000_load_openMSE_packages.R
@@ -0,0 +1,13 @@
+#####@> MSEtool (3.7.9999)... Github Development Version
+pak::pkg_install("blue-matter/MSEtool")
+
+#####@> SAMtool (1.8.1)...
+pak::pkg_install("blue-matter/SAMtool")
+
+#####@> openMSE (1.0.1)...
+pak::pkg_install("blue-matter/openMSE")
+
+#####@> nswo-mse (0.37.0)...
+pak::pkg_install("ICCAT/nswo-mse")
+
+
