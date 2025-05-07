094a684d8c1817c7a941faef0caaae4cbec04bac
Adrian Hordyk <ar.hordyk@gmail.com>
2025-05-07 19:03:34 -0300

    Update 000_load_openMSE_packages.R

--- 000_load_openMSE_packages.R
+++ 000_load_openMSE_packages.R
@@ -7,8 +7,8 @@ pak::pkg_install("blue-matter/SAMtool")
 #####@> openMSE (1.0.1)...
 pak::pkg_install("blue-matter/openMSE")
 
-#####@> nswo-mse (0.37.0)...
-pak::pkg_install("ICCAT/nswo-mse")
+# #####@> nswo-mse (0.37.0)...
+# pak::pkg_install("ICCAT/nswo-mse")
 
 
 pak::pkg_install("r4ss/r4ss")
