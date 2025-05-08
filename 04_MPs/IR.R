de97a53545f261fb015be03d56b47fc19b2f9e9b
Adrian Hordyk <ar.hordyk@gmail.com>
2025-05-08 16:46:34 -0300

    Update IR.R

--- 04_MPs/IR.R
+++ 04_MPs/IR.R
@@ -5,10 +5,18 @@ source('03AA_MP_Internal_Functions.R')
 
 # mc - maximum change in TAC between management cycles 
 
-IndexRatio <- function(x, Data, Data_Lag = 1, Interval = 3,
-                  Initial_MP_Yr = 2026, reps =  1, 
-                  tunepar = 1.143986, mc = c(0.25, 0.2), 
-                  yrs = c(3, 5), ...) {
+IndexRatio <- function(x,
+                       Data, 
+                       Data_Lag = 1, 
+                       Interval = 3,
+                       Initial_MP_Yr = 2026, 
+                       reps =  1, 
+                       tunepar = 0.9, 
+                       modifier = 1,
+                       mc = c(0.25, 0.25), 
+                       yrs = c(3, 3), 
+                       refYear=2020,
+                       ...) {
   Rec <- new("Rec")
   
   # Check if TAC needs to be updated
@@ -30,18 +38,25 @@ IndexRatio <- function(x, Data, Data_Lag = 1, Interval = 3,
   # Data@Ind[x,] <- smoothed_index
   
   ## Historical data...
-  yr.ind <- which(Data@Year == max(Data@Year) - yrs[1])
-  hist.yrs <- (yr.ind - yrs[2] + 1):yr.ind
-  I.hist <- mean(Data@Ind[x, hist.yrs], na.rm = TRUE) * tunepar
-  ## Current data...
-  curr.yrs <- (yr.ind + 1):(yr.ind + yrs[1])
-  I.curr <- mean(Data@Ind[x, curr.yrs], na.rm = TRUE)
-  ## Alpha...
-  alpha <- I.curr / I.hist 
-  ## Catch data...
-  Cat <- Data@Cat[x, length(Data@Cat[x, ])]
-  Cc <- MSEtool::trlnorm(reps = 1, Cat, Data@CV_Cat[x, 1])
+  yr.ind <- which(Data@Year == refYear)
+  hist.yrs <- (yr.ind - yrs[1] + 1):yr.ind
+  I.hist <- mean(Data@Ind[x, hist.yrs], na.rm = TRUE) * modifier
   
+  ## Current data...
+  yr.ind <- which.max(Data@Year)
+  curr.yrs <- (yr.ind - yrs[2]+ 1):yr.ind 
+  I.curr <- mean(Data@Ind[x, curr.yrs], na.rm = TRUE)
+  
+  # plot(Data@Year, Data@Ind[1,], type='l')
+  # lines(Data@Year[hist.yrs],Data@Ind[x, hist.yrs], col='blue', lwd=2)
+  # lines(Data@Year[curr.yrs],Data@Ind[x, curr.yrs], col='red', lwd=2)
+  #
+  ## Alpha...
+  alpha <- I.curr / I.hist  * tunepar
+  
+  ## Catch data...
+  Cat <- mean(Data@Cat[x, curr.yrs])
+ 
   ## Harvest control rule...
   # if(alpha > 1.2) {
   #   alpha <- 1.2
@@ -50,8 +65,8 @@ IndexRatio <- function(x, Data, Data_Lag = 1, Interval = 3,
   # } else {
   #   alpha <- alpha
   # }
-  ## TAC...
-  TAC <- alpha  * Cc
+ 
+  TAC <- alpha  * Cat
   ## TAC <- MSEtool::TACfilter(TAC)
   ## Return TAC...
   ## Rec@TAC <- TAC
@@ -59,4 +74,29 @@ IndexRatio <- function(x, Data, Data_Lag = 1, Interval = 3,
   
   Rec
 }
-class(IR_01) <- "MP"
+
+
+
+# Results:
+# - doSmoother = TRUE - worse performance
+# - incHCR = TRUE worse performance
+
+IR1 <- IndexRatio
+class(IR1) <- 'MP'
+
+IR2 <- IndexRatio
+formals(IR2)$mc <- c(0.2,0.2)  
+class(IR2) <- 'MP'
+
+IR3 <- IndexRatio
+formals(IR3)$mc <- c(0.15,0.15)  
+class(IR3) <- 'MP'
+
+
+# Test different configurations of function arguments
+# HistList <- readRDS("03_Hists/HistList.rda")
+# 
+# Hist <- HistList[[1]]
+# 
+# MSE <- Project(Hist, MPs=c('IR1', 'IR2', 'IR3'))
+# Pplot(MSE)
