fd890d0d392cc35aa2133c80d90ce4f73da8981b
Adrian Hordyk <ar.hordyk@gmail.com>
2025-05-07 21:19:00 -0300

    Update 05_script_project_Hists_ver00.R

--- 05_script_project_Hists_ver00.R
+++ 05_script_project_Hists_ver00.R
@@ -23,37 +23,37 @@
 ######@> Update openMSE R packages...
 
 #####@> MSEtool (3.7.2)...
-if(packageVersion("MSEtool") == "3.7.2") {
+if(packageVersion("MSEtool") == "3.7.9999") {
     print("MSEtool R package already installed is the correct version")
 } else {
     pak::pkg_install("blue-matter/MSEtool")
 }
 
 #####@> SAMtool (1.7.0)...
-if(packageVersion("SAMtool") == "1.7.0") {
+if(packageVersion("SAMtool") == "1.8.1") {
     print("SAMtool R package already installed is the correct version")
 } else {
     pak::pkg_install("blue-matter/SAMtool")
 }
 
 #####@> openMSE (1.1.1)...
-if(packageVersion("openMSE") == "1.1.1") {
+if(packageVersion("openMSE") == "1.0.1") {
     print("openMSE R package already installed is the correct version")
 } else {
     pak::pkg_install("blue-matter/openMSE")
 }
 
-#####@> nswo-mse (0.29.0)...
-if(packageVersion("SWOMSE") == "0.29.0") {
-    print("SWOMSE R package already installed is the correct version")
-} else {
-    pak::pkg_install("ICCAT/nswo-mse")
-}
+# #####@> nswo-mse (0.29.0)...
+# if(packageVersion("SWOMSE") == "0.29.0") {
+#     print("SWOMSE R package already installed is the correct version")
+# } else {
+#     pak::pkg_install("ICCAT/nswo-mse")
+# }
 
 ######@> Package list...
 library(dplyr)
 library(openMSE)
-library(ggmse)
+# library(ggmse)
 
 ######@> ggplot theme...
 extrafont::loadfonts(device = "postscript")
@@ -94,32 +94,51 @@ path02 <- "../R-Work/"
 ########################################################################
 ######@> Loading data...
 
-######@> Loading history objects...
-Hists <- sapply(dir("03_Hists",
-                    pattern = "IVInds_CORRECTED_ver03",
-                    full.names = TRUE), readRDS)
-
-####@> Setting names for Hists...
-names(Hists) <- paste0("OM", sprintf("%03d", 1:9))
-
-######@> Loading MPs...
-source("03_script_prepare_MPs_ver00.R")
-MPs <- c("FMSYref", "FMSYref75", "FMSYref110", ## Reference FMSY
-         "CE_01", "CE_02", "CE_03", ## Index-based: Exploitation rate
-         "IR_01", "IR_02", "IR_03", ## Index-based: I_ratio
-         "IS_01", "IS_02", "IS_03", ## Index-based: I_slope
-         "SP_01", "SP_02", "SP_03", "SP_04") ## Model-based: SP
+# ######@> Loading history objects...
+# Hists <- sapply(dir("03_Hists",
+#                     pattern = "IVInds_CORRECTED_ver03",
+#                     full.names = TRUE), readRDS)
+# 
+# ####@> Setting names for Hists...
+# names(Hists) <- paste0("OM", sprintf("%03d", 1:9))
+# 
+# ######@> Loading MPs...
+# source("03_script_prepare_MPs_ver00.R")
+# MPs <- c("FMSYref", "FMSYref75", "FMSYref110", ## Reference FMSY
+#          "CE_01", "CE_02", "CE_03", ## Index-based: Exploitation rate
+#          "IR_01", "IR_02", "IR_03", ## Index-based: I_ratio
+#          "IS_01", "IS_02", "IS_03", ## Index-based: I_slope
+#          "SP_01", "SP_02", "SP_03", "SP_04") ## Model-based: SP
 
 ########################################################################
 ######@> Simulating Projections Data...
 
+Hists <- readRDS("03_Hists/HistList.rda")
+
+ManagementOption <- 'DataLag_1_Interval_3'
+
+tunedMPs <- list.files(file.path('TunedMPs', ManagementOption), full.names = TRUE)
+
+MPnames <- gsub('.mp', '', basename(tunedMPs))
+
+
+for (i in seq_along(tunedMPs)) {
+  mp <- readRDS(tunedMPs[i])
+  assign(MPnames[i], mp, envir=.GlobalEnv)
+}
+
+
 ######@> Looping to run the MSE for all histories data...
 for(i in seq_along(Hists)) {
     Hist <- Hists[[i]]
-    MSE <- Project(Hist, MPs = MPs, parallel = FALSE)
-    nm <- paste0("MSE", sprintf("%03d", i), "FULL_IVInds_CORRECTED",
-                 "_00.mse")
-    saveRDS(MSE, file.path("04_MSEs", nm))
+    nm <- gsub('OM', '', Hist@OM@Name) |> trimws() 
+    nm <- paste(sprintf("%03d", i), nm, sep="_")
+    nm <- paste0(nm, '.mse')
+    if (!dir.exists(file.path("04_MSEs", ManagementOption)))
+      dir.create(file.path("04_MSEs", ManagementOption))
+    
+    MSE <- Project(Hist, MPs = MPnames, parallel = FALSE)
+    saveRDS(MSE, file.path("04_MSEs", ManagementOption, nm))
 }
 
 ########################################################################
