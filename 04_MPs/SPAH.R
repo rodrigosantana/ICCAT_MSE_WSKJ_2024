9239ae51cddaa55f37f58416da4d3e92a2cf11c2
Adrian Hordyk <ar.hordyk@gmail.com>
2025-05-09 11:40:13 -0300

    Update SPAH.R

--- 04_MPs/SPAH.R
+++ 04_MPs/SPAH.R
@@ -6,10 +6,13 @@ SurplusProductionStockStatus <- function(x, Data,
                                  Interval = 3,
                                  Initial_MP_Yr = 2026, 
                                  reps =  1, 
-                                 tunepar = 0.5,
+                                 tunepar = 1,
                                  mc = c(0.25, 0.25),
-                                 n=3,
-                                 BMSYTarg=1.2,
+                                 useHCR=TRUE,
+                                 BMSYTarg=1.3,
+                                 BMSYLim=0.5,
+                                 delta1=1,
+                                 delta2=0.5,
                                  ...) {
   Rec <- new("Rec")
   
@@ -32,17 +35,58 @@ SurplusProductionStockStatus <- function(x, Data,
   
   # Slope to Target Rule 
   EstB_BMSY <- SPrun@B_BMSY
-  EstSS <-  tail(EstB_BMSY,2) |> mean()
-
-  delta <- 1+((EstSS - BMSYTarg)/n* tunepar)
+  EstSS <-  tail(EstB_BMSY,1) 
+  
+  # ##############################################################################
+  # 
+  # par(mfrow=c(2,2))
+  # B <- c(rowSums(HistList[[om]]@TSdata$Biomass[x,,]), 
+  #   MSEList[[om]]@B[x,mm,])
+  # B_BMSY <- B/HistList[[om]]@Ref$ByYear$BMSY
+  # 
+  # 
+  # plot(Data@Year, B_BMSY[seq_along(Data@Year)], type='l', ylim=c(0,3))
+  # lines(Data@Year, EstB_BMSY[seq_along(Data@Year)], col='red')
+  # 
+  # plot(Data@Year, Data@Cat[x,], type='l', ylim=c(0,max(Data@Cat[x,])))
+  # 
+  # plot(Data@Year, Data@Ind[x,], type='l', ylim=c(0,2))
+  # 
+  # ##############################################################################
+  
+  # delta <- 1+((EstSS - BMSYTarg)/n* tunepar)
+  delta <- 1+(EstSS - BMSYTarg) * tunepar
 
   # yind <- seq_along(EstSS)
   # summary(lm(EstSS~yind))$coefficients[2, 1:2]
   
-  TAC <- delta  * Data@MPrec[x]
+  prevER <- tail(Data@Cat[x,],1)/tail(EstB_BMSY,2)[1] 
+ 
+  Bratio <- EstSS/BMSYTarg
+  if (useHCR) {
+    if (Bratio>=1) {
+      delta <- delta
+    } else if (EstSS>=BMSYLim) {
+      a <- (delta1-delta2)/(BMSYTarg-BMSYLim)
+      b <- deltaMin -a*BMSYLim
+      delta <- a*Bratio+b
+      # Bratio <- seq(BMSYLim, BMSYTarg, by=0.1)
+      # plot(Bratio, a*Bratio+b, type='l', ylim=c(0,1))
+    } else {
+      delta <- deltaMin
+    }
+  }
+  
+  newER <- prevER * delta
+  TAC <- newER*tail(EstB_BMSY,1)[1]
+  
   ## Maximum allowed change in TAC
   ## Rec@TAC <- TAC
-  Rec@TAC <- adjust_TAC(TAC, Data@MPrec[x], mc)
+  if (Bratio>=1) {
+    TAC <- adjust_TAC(TAC, Data@MPrec[x], mc)
+  }
+  
+  Rec@TAC <- TAC
   Rec
 }
 
@@ -51,22 +95,42 @@ SPAH1 <- SurplusProductionStockStatus
 class(SPAH1) <- 'MP'
 
 SPAH2 <- SurplusProductionStockStatus
-formals(SPAH2)$n <- 4
+formals(SPAH2)$BMSYLim <- 1
 class(SPAH2) <- 'MP'
 
 SPAH3 <- SurplusProductionStockStatus
-formals(SPAH3)$tunepar <- 0.3
+formals(SPAH3)$BMSYLim <- 0.8
 class(SPAH3) <- 'MP'
 
 
 # Test different configurations of function arguments
 HistList <- readRDS("03_Hists/HistList.rda")
 
-Hist <- HistList[[1]]
+selOMs <- c(1,5,9)
 
-MSE <- Project(Hist, MPs=c('SPAH1', 'SPAH2', 'SPAH3'))
-Pplot(MSE)
+MSEList <- list()
+for (i in selOMs) {
+  MSEList[[i]] <- Project(HistList[[i]], MPs=c('SPAH1', 'SPAH2', 'SPAH3'))
+  Pplot(MSEList[[i]])
+}
 
-# Results:
-# - doSmoother = TRUE - worse performance
-# - incHCR = TRUE worse performance
+
+
+
+pyears <- 2025:2054 
+cbind(1:30, pyears)
+
+pyears[15]
+
+seq(2026, by = 3, length.out = 30)
+
+
+Pplot(MSE1)
+Pplot(MSE5)
+Pplot(MSE9)
+
+MSE5@SB_SBMSY[,1,15] |> which.min()
+x <- 56
+mm <- 1
+om <- 5
+Data <- SWOMSE::Trim_Data(MSE5@PPD[[mm]], 2034)
